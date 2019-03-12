0f56902b7f4499c4389094cc821d52fb14d39dbc
Vera Butler <verabutlerdesign@gmail.com>
2019-03-11 22:33:21 -0400

    Updates

---
+++ precache-manifest.bdd56d98506b95719b486eab51ba8f33.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4736cba5670164179232",
+    "url": "/static/css/main.ef6ac475.chunk.css"
+  },
+  {
+    "revision": "4736cba5670164179232",
+    "url": "/static/js/main.4736cba5.chunk.js"
+  },
+  {
+    "revision": "29c4a7f765ba0ab505ff",
+    "url": "/static/js/runtime~main.29c4a7f7.js"
+  },
+  {
+    "revision": "b53fdf86951d19efb34c",
+    "url": "/static/js/2.b53fdf86.chunk.js"
+  },
+  {
+    "revision": "0fd785da4d209a43c6c6",
+    "url": "/static/js/3.0fd785da.chunk.js"
+  },
+  {
+    "revision": "c006da51ef894d0db19c",
+    "url": "/static/js/4.c006da51.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "507a60f2c3f807eff6bff09de20bb6f8",
+    "url": "/index.html"
+  }
+];
